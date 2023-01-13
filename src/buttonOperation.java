dc7a9a8bca9c3fee2f399a1d4b08dfea5b7d980b
delleviin <dellevin99@gmail.com>
2023-01-13 22:24:07 +0800

    新增文件读取的接口

---
+++ src/buttonOperation.java
@@ -0,0 +1,59 @@
+import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import java.awt.*;
+import java.io.File;
+
+public interface buttonOperation {
+    /*
+     * 打开文件
+     */
+    static void showFileOpenDialog(Component parent, JTextArea msgTextArea) {
+        // 创建一个默认的文件选取器
+        JFileChooser fileChooser = new JFileChooser();
+
+        // 设置默认显示的文件夹为当前文件夹
+        fileChooser.setCurrentDirectory(new File("."));
+
+        // 设置文件选择的模式（只选文件、只选文件夹、文件和文件均可选）
+        fileChooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+        // 设置是否允许多选
+        fileChooser.setMultiSelectionEnabled(true);
+
+        // 添加可用的文件过滤器（FileNameExtensionFilter 的第一个参数是描述, 后面是需要过滤的文件扩展名 可变参数）
+        fileChooser.addChoosableFileFilter(new FileNameExtensionFilter("zip(*.zip, *.rar)", "zip", "rar"));
+        // 设置默认使用的文件过滤器
+        fileChooser.setFileFilter(new FileNameExtensionFilter("image(*.jpg, *.png, *.gif)", "jpg", "png", "gif"));
+
+        // 打开文件选择框（线程将被阻塞, 直到选择框被关闭）
+        int result = fileChooser.showOpenDialog(parent);
+
+        if (result == JFileChooser.APPROVE_OPTION) {
+            // 如果点击了"确定", 则获取选择的文件路径
+            File file = fileChooser.getSelectedFile();
+
+            // 如果允许选择多个文件, 则通过下面方法获取选择的所有文件
+            // File[] files = fileChooser.getSelectedFiles();
+
+            msgTextArea.append( file.getAbsolutePath());
+        }
+    }
+    /*
+     * 选择文件保存路径
+     */
+    private static void showFileSaveDialog(Component parent, JTextArea msgTextArea) {
+        // 创建一个默认的文件选取器
+        JFileChooser fileChooser = new JFileChooser();
+
+        // 设置打开文件选择框后默认输入的文件名
+        fileChooser.setSelectedFile(new File("测试文件.zip"));
+
+        // 打开文件选择框（线程将被阻塞, 直到选择框被关闭）
+        int result = fileChooser.showSaveDialog(parent);
+
+        if (result == JFileChooser.APPROVE_OPTION) {
+            // 如果点击了"保存", 则获取选择的保存路径
+            File file = fileChooser.getSelectedFile();
+            msgTextArea.append("保存到文件: " + file.getAbsolutePath() + "\n\n");
+        }
+    }
+}
